2a54113300f7eee0f920d87b71a1b9e532eca013
i-kamran <ibragimovkmrn@gmail.com>
2024-11-20 19:43:37 +0500

    feat: Add cleaned lsp config

--- configs/.config/nvim/lua/plugins/lsp-and-formatting.lua
+++ configs/.config/nvim/lua/plugins/lsp-and-formatting.lua
@@ -1,168 +1,165 @@
 -- ~/.config/nvim/lua/plugins/lsp-and-formatting.lua
 
 return {
-    -- lsp config
-    {
-        "neovim/nvim-lspconfig",
-        -- lazy = true,
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/nvim-cmp",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "j-hui/fidget.nvim",
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    -- LSP Support
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+    -- Autocompletion
+    "hrsh7th/nvim-cmp", -- Required
+    "hrsh7th/cmp-nvim-lsp", -- Required
+    "L3MON4D3/LuaSnip", -- Required
+    "rafamadriz/friendly-snippets",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  config = function()
+    ---
+    -- LSP setup
+    ---
+
+    vim.opt.signcolumn = "yes" -- Prevent layout shifts
+
+    -- Extend capabilities
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    local lspconfig = require("lspconfig")
+    local lspconfig_defaults = lspconfig.util.default_config
+    lspconfig_defaults.capabilities = vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, capabilities)
+
+    -- Mason setup
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
         },
-        config = function()
-            local cmp = require("cmp")
-            local cmp_lsp = require("cmp_nvim_lsp")
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_lsp.default_capabilities()
-            )
+      },
+    })
+    require("mason-tool-installer").setup({
 
-            require("fidget").setup({})
-            require("mason").setup({})
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "rust_analyzer",
-                    "gopls",
-                    "ts_ls",
-                    "cssls",
-                    "html",
-                    "clangd",
-                    "pyright",
-                    "marksman",
-                    "bashls",
-                    "tailwindcss",
-                },
-                automatic_installation = true,
-                handlers = {
-                    function(server_name)
-                        require("lspconfig")[server_name].setup({
-                            capabilities = capabilities,
-                        })
-                    end,
-                    ["pyright"] = function()
-                        require("lspconfig").pyright.setup({
-                            capabilities = capabilities,
-                            settings = {
-                                python = {
-                                    analysis = {
-                                        typeCheckingMode = "strict",
-                                    },
-                                },
-                            },
-                        })
-                    end,
-                },
-            })
+      -- a list of all tools you want to ensure are installed upon
+      -- start
+      ensure_installed = {
 
-            -- Snippets and completion setup
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                formatting = {
-                    format = require("nvim-highlight-colors").format
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ["<A-Tab>"] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer" },
-                }),
-            })
+        -- you can pin a tool to a particular version
+        -- { "golangci-lint", version = "v1.47.0" },
 
-            vim.diagnostic.config({
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
+        -- you can turn off/on auto_update per tool
+        -- { "bash-language-server", auto_update = true },
+
+        -- Formatters
+        "stylua",
+        "isort",
+        "black",
+        "prettierd",
+        "prettier",
+        "gofumpt",
+        "beautysh",
+        "clang-format",
+        "rustfmt",
+
+        -- Linters
+        "eslint_d",
+        "pylint",
+        "mypy",
+        "luacheck",
+        "golangci-lint",
+        "markdownlint",
+        "jsonlint",
+        "htmlhint",
+        "misspell",
+        "cpplint",
+
+        -- Language Servers
+        "lua-language-server",
+        "lua_ls",
+        "pylsp",
+        "gopls",
+        "jsonls",
+        "html",
+        "tailwindcss",
+        "dockerls",
+        "bash-language-server",
+        "marksman",
+        "rust-analyzer",
+        "clangd",
+
+        "debugpy",
+        "codelldb",
+        "delve",
+      },
+      auto_update = true,
+      run_on_start = true,
+    })
+    require("mason-lspconfig").setup({
+      handlers = {
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+            -- Add server-specific settings here
+            settings = {
+              -- Example for Lua LSP
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
                 },
-            })
+              },
+            },
+          })
         end,
-    },
+      },
+    })
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    -- none-ls config
-    {
-        "nvimtools/none-ls.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "jay-babu/mason-null-ls.nvim",
-            "nvimtools/none-ls-extras.nvim",
-        },
-        config = function()
-            local null_ls = require("null-ls")
-            local mason_null_ls = require("mason-null-ls")
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-            mason_null_ls.setup({
-                ensure_installed = {
-                    -- Formatters
-                    "prettierd",
-                    "stylua",
-                    "isort",
-                    "black",
-                    -- Linters
-                    "eslint_d",
-                    "flake8",
-                    "shellcheck",
-                    "markdownlint",
-                    "markdown-toc",
-                    -- DAP
-                    "debugpy" -- add it here as it won't be installed automatically 
-
-                    -- with mason from in DAP config
-                },
-                automatic_installation = true,
-            })
-
-            null_ls.setup({
-                sources = {
-                    -- Formatters
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.prettierd,
-                    null_ls.builtins.formatting.isort,
-                    null_ls.builtins.formatting.black,
-                    -- Linters
-                    require("none-ls.diagnostics.eslint_d"),
-                    require("none-ls.diagnostics.flake8"),
-                    -- Other
-                    null_ls.builtins.completion.spell,
-                    null_ls.builtins.diagnostics.markdownlint.with({
-                        diagnostics_format = "#{m} (#{c})",
-                    }),
-                },
-                on_attach = function(client, bufnr)
-                    local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
-                    if filetype == "c" or filetype == "cpp" then
-                        client.stop()
-                    end
-                end,
-            })
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
         end,
-    },
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      sources = {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip", priority = 750 },
+        { name = "buffer", priority = 500 },
+        { name = "path", priority = 250 },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<A-Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+      }),
+    })
+    -- `/` cmdline setup.
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
+
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+        { name = "cmdline" },
+      }),
+    })
+  end,
 }
