a7b899f930a98287e11e21e3b26683da244d5080
i-kamran <ibragimovkmrn@gmail.com>
2024-11-20 19:40:36 +0500

    feat: Add linting group

--- configs/.config/nvim/lua/set.lua
+++ configs/.config/nvim/lua/set.lua
@@ -36,14 +36,14 @@ vim.opt.spell = true
 vim.opt.conceallevel = 1 -- obsidian.nvim
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function()
-		vim.cmd("highlight Visual guibg=#26403D")
-	end,
+  pattern = "*",
+  callback = function()
+    vim.cmd("highlight Visual guibg=#26403D")
+  end,
 })
 
 local augroup_colorcolumn = vim.api.nvim_create_augroup("ColorColumn", { clear = true })
-vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, {
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
   group = augroup_colorcolumn,
   pattern = "*",
   callback = function()
@@ -65,35 +65,44 @@ vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, {
 
 local augroup_commit_spell = vim.api.nvim_create_augroup("CommitSpell", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-	group = augroup_commit_spell,
-	pattern = { "NeogitCommitMessage", "gitcommit" },
-	callback = function()
-		vim.opt.colorcolumn = "50"
-		vim.opt.spell = true
-	end,
+  group = augroup_commit_spell,
+  pattern = { "NeogitCommitMessage", "gitcommit" },
+  callback = function()
+    vim.opt.colorcolumn = "50"
+    vim.opt.spell = true
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function()
-		vim.cmd("highlight Comment guifg=#9f9f9f")
-	end,
+  pattern = "*",
+  callback = function()
+    vim.cmd("highlight Comment guifg=#9f9f9f")
+  end,
 })
 
 vim.api.nvim_create_autocmd("DiagnosticChanged", {
-	callback = function()
-		local current_bufnr = vim.api.nvim_get_current_buf()
-		local diagnostics = vim.diagnostic.get(current_bufnr)
-		local items = vim.diagnostic.toqflist(diagnostics)
-		vim.fn.setloclist(0, items, "r")
-	end,
+  callback = function()
+    local current_bufnr = vim.api.nvim_get_current_buf()
+    local diagnostics = vim.diagnostic.get(current_bufnr)
+    local items = vim.diagnostic.toqflist(diagnostics)
+    vim.fn.setloclist(0, items, "r")
+  end,
 })
 vim.api.nvim_create_user_command("MarkdownTOC", function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local filename = vim.api.nvim_buf_get_name(bufnr)
-    -- Run markdown-toc on the current buffer
-    vim.fn.system(string.format("markdown-toc -i %s", filename))
-    -- Optionally, you can reload the buffer to see changes
-    vim.cmd("edit")
+  local bufnr = vim.api.nvim_get_current_buf()
+  local filename = vim.api.nvim_buf_get_name(bufnr)
+  -- Run markdown-toc on the current buffer
+  vim.fn.system(string.format("markdown-toc -i %s", filename))
+  -- Optionally, you can reload the buffer to see changes
+  vim.cmd("edit")
 end, {})
 
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+-- Add additional events to trigger linting dynamically
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+  group = lint_augroup,
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
