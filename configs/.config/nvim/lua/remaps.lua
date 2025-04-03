9af77fa77793a2d94d241135c2f97fbade269c0b
i-kamran <ibragimovkmrn@gmail.com>
2025-04-03 23:51:03 +0500

    chore: Update remaps

--- configs/.config/nvim/lua/remaps.lua
+++ configs/.config/nvim/lua/remaps.lua
@@ -14,7 +14,6 @@ vim.keymap.set("n", "<leader>mm", "<cmd>lua MiniFiles.open()<CR>", { desc = "[m]
 vim.keymap.set("i", "<A-j>", "<Esc>")
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "e[x]ecutable" })
 vim.keymap.set("n", "<leader>ww", "<cmd>w<CR>", { noremap = false, desc = "[w]rite" })
-vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>wa", "<cmd>wa<cr>", { noremap = false, desc = "[w]rite [a]ll" })
 vim.keymap.set("n", "<leader>ee", "<cmd>q!<cr>", { noremap = false, desc = "[e]xit" })
 vim.keymap.set("n", "<leader>ea", "<cmd>qa!<CR>", { noremap = false, desc = "[e]xit [a]ll" })
@@ -40,15 +39,10 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "[y]ank to clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "[Y]ank line to clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "[d]elete over" }) --  without overwriting register
 
--- Quickfix and Location lists Navigation
-vim.keymap.set("n", "<leader>lo", "<cmd>lopen<CR>zz", { desc = "[l]ocation [o]pen" })
-vim.keymap.set("n", "<leader>lc", "<cmd>lclose<CR>zz", { desc = "[l]ocation [c]lose" })
-vim.keymap.set("n", "<leader>lk", "<cmd>lnext<CR>zz", { desc = "[l]ocation next" })
-vim.keymap.set("n", "<leader>lj", "<cmd>lprev<CR>zz", { desc = "[l]ocation prev" })
+
+-- QuickFix, Tag match-list, Argument list,  Buffer list
 vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>", { desc = "[q]uickfix [o]pen" })
-vim.keymap.set("n", "<leader>qc", "<cmd>cclose<CR>", { desc = "[q]uickfix [c]lose" })
-vim.keymap.set("n", "<leader>qk", "<cmd>cprev<CR>", { desc = "[q]uickfix prev" })
-vim.keymap.set("n", "<leader>qj", "<cmd>cnext<CR>", { desc = "[q]uickfix next" })
+
 
 -- Tmux Integration Keymaps
 vim.keymap.set("n", "<C-t>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -58,12 +52,7 @@ vim.keymap.set("n", "<C-k>", vim.cmd.NvimTmuxNavigateUp)
 vim.keymap.set("n", "<C-l>", vim.cmd.NvimTmuxNavigateRight)
 
 -- Search and Replace
-vim.keymap.set(
-  "n",
-  "<leader>ss",
-  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-  { desc = "replace under curson" }
-)
+vim.keymap.set("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace under curson" })
 
 -- Undo Tree Toggle
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "[u]ndo tree" })
@@ -78,19 +67,16 @@ vim.keymap.set("n", "<leader>gps", "<cmd>Neogit push<CR>", { desc = "neo[g]it [p
 vim.keymap.set("n", "<leader>gpl", "<cmd>Neogit pull<CR>", { desc = "neo[g]it [p]u[l]l" })
 
 -- LSP Keymaps
-vim.keymap.set("n", "<A-f>", function()
-  require("conform").format()
-end)
+vim.keymap.set("n", "<A-f>", function() require("conform").format() end)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
 
-vim.keymap.set("n", "<leader>ln", function()
-  require("lint").try_lint()
-end, { desc = "[l]i[n]t" })
+vim.keymap.set("n", "<leader>ln", function() require("lint").try_lint() end, { desc = "[l]i[n]t" })
 vim.keymap.set("n", "grd", vim.lsp.buf.definition, { desc = "[g]o to [d]efinition" })
 vim.keymap.set("n", "grD", vim.lsp.buf.declaration, { desc = "[g]o to [D]eclaration" })
 vim.keymap.set("n", "grt", vim.lsp.buf.type_definition, { desc = "[g]o to [t]ype definition" })
 --
 vim.keymap.set("n", "<leader>lf", vim.diagnostic.open_float, { desc = "diagnostic [f]loat" })
+vim.keymap.set("n", "<leader>qo", "<cmd>copen<CR>", { desc = "[q]uickfix [o]pen" })
 vim.keymap.set("n", "<leader>ll", vim.diagnostic.setloclist, { desc = "diagnostics [l]ist" })
 vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR>", { desc = "[l]sp [r]estart" })
 vim.keymap.set("n", "<leader>ls", "<cmd>LspStart<CR>", { desc = "[l]sp [s]tart" })
@@ -113,12 +99,8 @@ vim.keymap.set("n", "<leader>tj", vim.cmd.TSJToggle, { desc = "[t]rees[j]" })
 vim.keymap.set("n", "<leader>tx", "<cmd>Trouble diagnostics toggle<CR>", { desc = "[t]rouble" })
 
 -- Flash
-vim.keymap.set({ "n", "x", "o" }, "s", function()
-  require("flash").jump()
-end, { desc = "Flash" })
-vim.keymap.set({ "n", "x", "o" }, "S", function()
-  require("flash").treesitter()
-end, { desc = "Flash Treesitter" })
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
 
 -- Snippets
 vim.keymap.set("n", "<leader>sr", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { desc = "[s]nippet e[r]ror" }) -- Insert Go error handling snippet
@@ -145,9 +127,7 @@ vim.keymap.set("n", "<leader>st", function()
 end)
 
 -- Hide terminal with <Esc><Esc> in terminal mode
-vim.keymap.set("t", "<Esc><Esc>", function()
-  vim.cmd("hide")
-end)
+vim.keymap.set("t", "<Esc><Esc>", function() vim.cmd("hide") end)
 
 -- DAP
 vim.keymap.set("n", "2", require("dap").continue)
@@ -167,59 +147,23 @@ vim.keymap.set("n", "<leader>fm", ":Telescope harpoon marks<CR>", { desc = "harp
 vim.keymap.set("n", "<leader>fst", require("telescope.builtin").git_status, { desc = "[f]ind [st]atus" })
 vim.keymap.set("n", "<Leader>ft", ":Telescope git_worktree git_worktrees<CR>", { desc = "[f]ind [t]ree" })
 vim.keymap.set("n", "<Leader>fT", ":Telescope git_worktree create_git_worktree<CR>", { desc = "Create [t]ree" })
-vim.keymap.set("n", "<leader>fsg", function()
-  require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
-end, { desc = "[f]ind [s]tring" })
-vim.keymap.set("n", "<leader>fw", function()
-  local word = vim.fn.expand("<cword>")
-  require("telescope.builtin").grep_string({ search = word })
-end, { desc = "[f]ind [w]ord" })
-vim.keymap.set("n", "<leader>fW", function()
-  local word = vim.fn.expand("<cWORD>")
-  require("telescope.builtin").grep_string({ search = word })
-end, { desc = "[f]ind [W]ORD" })
+vim.keymap.set("n", "<leader>fsg", function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end, { desc = "[f]ind [s]tring" })
+vim.keymap.set("n", "<leader>fw", function() local word = vim.fn.expand("<cword>") require("telescope.builtin").grep_string({ search = word }) end, { desc = "[f]ind [w]ord" })
+vim.keymap.set("n", "<leader>fW", function() local word = vim.fn.expand("<cWORD>") require("telescope.builtin").grep_string({ search = word }) end, { desc = "[f]ind [W]ORD" })
 
 -- Harpoon
-vim.keymap.set("n", "<A-a>", function()
-  require("harpoon"):list():add()
-end, { desc = "[a]dd harpoon" })
-vim.keymap.set("n", "<A-e>", function()
-  require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
-end)
-vim.keymap.set("n", "<A-1>", function()
-  require("harpoon"):list():select(1)
-end)
-vim.keymap.set("n", "<A-2>", function()
-  require("harpoon"):list():select(2)
-end)
-vim.keymap.set("n", "<A-3>", function()
-  require("harpoon"):list():select(3)
-end)
-vim.keymap.set("n", "<A-4>", function()
-  require("harpoon"):list():select(4)
-end)
-vim.keymap.set("n", "<A-5>", function()
-  require("harpoon"):list():select(5)
-end)
+vim.keymap.set("n", "<A-a>", function() require("harpoon"):list():add() end, { desc = "[a]dd harpoon" })
+vim.keymap.set("n", "<A-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end)
+vim.keymap.set("n", "<A-1>", function() require("harpoon"):list():select(1) end)
+vim.keymap.set("n", "<A-2>", function() require("harpoon"):list():select(2) end)
+vim.keymap.set("n", "<A-3>", function() require("harpoon"):list():select(3) end)
+vim.keymap.set("n", "<A-4>", function() require("harpoon"):list():select(4) end)
+vim.keymap.set("n", "<A-5>", function() require("harpoon"):list():select(5) end)
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<A-S-P>", function()
-  require("harpoon"):list():prev()
-end)
-vim.keymap.set("n", "<A-S-N>", function()
-  require("harpoon"):list():prev()
-end)
-vim.keymap.set("n", "<leader>A-1>", function()
-  require("harpoon"):list():replace_at(1)
-end)
-vim.keymap.set("n", "<leader>A-2>", function()
-  require("harpoon"):list():replace_at(2)
-end)
-vim.keymap.set("n", "<leader>A-3>", function()
-  require("harpoon"):list():replace_at(3)
-end)
-vim.keymap.set("n", "<leader>A-4>", function()
-  require("harpoon"):list():replace_at(4)
-end)
-vim.keymap.set("n", "<leader>A-5>", function()
-  require("harpoon"):list():replace_at(5)
-end)
+vim.keymap.set("n", "<A-S-P>", function() require("harpoon"):list():prev() end)
+vim.keymap.set("n", "<A-S-N>", function() require("harpoon"):list():prev() end)
+vim.keymap.set("n", "<leader>A-1>", function() require("harpoon"):list():replace_at(1) end)
+vim.keymap.set("n", "<leader>A-2>", function() require("harpoon"):list():replace_at(2) end)
+vim.keymap.set("n", "<leader>A-3>", function() require("harpoon"):list():replace_at(3) end)
+vim.keymap.set("n", "<leader>A-4>", function() require("harpoon"):list():replace_at(4) end)
+vim.keymap.set("n", "<leader>A-5>", function() require("harpoon"):list():replace_at(5) end)
