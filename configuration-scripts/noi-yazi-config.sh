ad25ba1298e2252a34f4e6ac734dc53ead641676
i-kamran <ibragimovkmrn@gmail.com>
2025-04-28 16:37:25 +0500

    feat: Add yazi plugin install

--- configuration-scripts/noi-yazi-config.sh
+++ configuration-scripts/noi-yazi-config.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-###################
-### Noi Wayland ###
-###################
+###########################
+### Noi and Yazi config ###
+###########################
 
-# Script creates a wrapper for noi-desktop to enable Wayland support
+# Script creates a wrapper for noi-desktop to enable Wayland support,
+# installs Yazi plugins and set default mime types
 
-echo "Enabling Noi Wayland support"
+echo "Enabling Noi Wayland support and installing Yazi plugins"
 
 # Create the wrapper script
 sudo bash -c 'cat > /usr/local/bin/noi-desktop << EOF
@@ -17,4 +18,12 @@ EOF'
 # Make the wrapper script executable
 sudo chmod +x /usr/local/bin/noi-desktop
 
-echo "Noi Wayland support enabled successfully"
+# Set Gwenview as default for jpeg, png, webp
+xdg-mime default org.kde.gwenview.desktop image/jpeg image/png image/webp
+
+# Downdload yazi mount and git plugins
+ya pack -a yazi-rs/plugins:git
+ya pack -a yazi-rs/plugins:mount
+
+echo "Script finished successfully"
+
