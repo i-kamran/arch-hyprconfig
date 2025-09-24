da083820e55ce2d39243b9a7be504298d0f62db6
i-kamran <ibragimovkmrn@gmail.com>
2025-09-24 17:53:05 +0500

    feat(aliases.zsh): Add eza aliases

--- configs/.config/zsh/aliases.zsh
+++ configs/.config/zsh/aliases.zsh
@@ -9,15 +9,20 @@ alias cx="clear"                                                              # 
 alias cpf="copyfile"                                                          # copyfile zsh plugin
 alias sht="shutdown now"                                                      # Shutdown
 alias re="reboot"                                                             # Reboot
+alias dolphinenable="systemctl --user start kbuildsycoca.service"             # Enable dolphin default apps
 alias de="dolphinenable"                                                      # Execute script to fix dolphin
 alias ts="tmux-sessionizer"                                                   # Execute tmux-sessionizer script
 alias cs="~/.config/auto-commit.sh"                                           # Auto commit script
 alias chwp="~/.config/change-wallpaper.sh"                                    # Script to change wallpaper
 alias addw="~/.config/aspell-add-word.sh"                                     # Script to add words into personal aspell dict
-alias dolphinenable="systemctl --user start kbuildsycoca.service"             # Enable dolphin default apps
 alias python="~/.venv/bin/python"
 alias pip="~/.venv/bin/pip"
 
+
+# Eza
+alias ls="eza"
+alias l="eza -l --no-permissions --icons --no-user --git -a --no-filesize --group-directories-first "
+alias lt="eza --tree --level=2 --long --icons --git"
 
 
 # Zsh configuration management
