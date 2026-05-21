ed7b026f43bae2705cb6bf1d4677f20182894d86
i-kamran <ibragimovkmrn@gmail.com>
2026-05-21 23:39:54 +0500

    feat: Update treesitter config

--- configs/.config/nvim/lua/plugins/treesitter.lua
+++ configs/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,43 @@
 -- ~/.config/nvim/lua/plugins/treesitter.lua
 
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "python" },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-            additional_vim_regex_highlighting = false,
-        })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldenable = false
+      end,
+    })
+  end,
+  config = function()
+    local installed = require("nvim-treesitter.config").get_installed()
+    local wanted = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "javascript",
+      "html",
+      "python",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "json",
+      "toml",
+      "gitcommit"
+    }
+    local to_install = vim.tbl_filter(function(p)
+      return not vim.tbl_contains(installed, p)
+    end, wanted)
+    if #to_install > 0 then
+      require("nvim-treesitter").install(to_install)
+    end
+  end,
 }
